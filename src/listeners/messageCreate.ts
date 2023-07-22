b35c24531587ce337efb36662b35fb0296914760
saigyou_ayakashi <minuit1993@live.fr>
2023-07-23 00:03:27 +0200

    Add getLogger in messageCreate

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,11 +1,13 @@
 import { ChannelType, Client, Message } from "discord.js";
+import { getLogger } from "../utils/getLogger";
 
 export const messageCreate = {
   name: "messageCreate",
 
   execute: async (client: Client, message: Message) => {
     if (message.channel.type === ChannelType.DM) {
-      console.log(`${message.author.tag} sent a DM: ${message.content} in DM`);
+      const log = getLogger("Bot");
+      log.info(`DM from ${message.author.tag}: ${message.content}`);
       return;
     }
   },
